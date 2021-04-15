1816009a71c26bf601fcd3ac9150a0b111e45ed4
zydrunas <valciukaszyd@gmail.com>
2021-04-16 00:16:59 +0800

    barboros portfolio

---
+++ src/components/Navbar/MenuItems.js
@@ -0,0 +1,27 @@
+export const MenuItems = [
+  {
+    title: "Pagrindinis",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Kiti darbai",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Dar kiti darbai",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Kontaktai",
+    url: "#",
+    cName: "nav-links",
+  },
+  {
+    title: "Sign Up",
+    url: "#",
+    cName: "nav-links-mobile",
+  },
+];
